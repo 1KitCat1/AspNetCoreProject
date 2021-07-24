c3746a713e931bf91bcf581cf1454edd355a9c13
1KitCat1 <denischool119@gmail.com>
2021-07-24 22:44:43 +0300

    Add Entity items

---
+++ AspNetCoreProject/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreProject.Domain.Entities
+{
+    public class ServiceItem : EntityBase
+    {
+        [Required(ErrorMessage = "Введите название услуги")]
+        [Display(Name = "Название услуги")]
+        public override string Title { get; set; } = "Название услуги";
+        [Display(Name = "Краткое описание услуги")]
+        public override string Subtitle { get; set; } = "Краткое описание";
+
+        [Display(Name = "Описание услуги")]
+        public override string Text { get; set; } = "Описание услуги";
+
+    }
+}

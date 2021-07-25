e76ada34963576d91a43c183f3781c846b1eb99e
1KitCat1 <denischool119@gmail.com>
2021-07-25 22:35:25 +0300

    Add DataManager

--- AspNetCoreProject/Domain/AppDbContext.cs
+++ AspNetCoreProject/Domain/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using AspNetCoreProject.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,10 @@ namespace AspNetCoreProject.Domain
     public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<TextField> TextFields { get; set; }
+        public DbSet<ServiceItem> ServiceItems { get; set; }
+
+
 
     }
 }

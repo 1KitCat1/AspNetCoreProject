9187db08a4cabc2b45e3be0ce4cbb52034d982ff
1KitCat1 <denischool119@gmail.com>
2021-08-06 21:34:56 +0300

    Add view Login

--- AspNetCoreProject/Models/LoginViewModel.cs
+++ AspNetCoreProject/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace AspNetCoreProject.Models
 {
     public class LoginViewModel
     {
+        [Required]
+        [Display(Name ="Login")]
+        public string UserName { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "RememberMe")]
+        public bool RememberMe { get; set; }
+
     }
 }
